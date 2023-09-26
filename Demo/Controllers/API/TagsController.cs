54a8b6b6016a49410e46f4cf4680186409982329
Elnur Mammadli <elnur@elforte.ca>
2023-09-26 04:57:52 -0600

    react added

---
+++ Demo/Controllers/API/TagsController.cs
@@ -0,0 +1,24 @@
+﻿using Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Controllers.API
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TagsController : ControllerBase
+	{
+		private readonly ApplicationDbContext _dbContext;
+
+		public TagsController(ApplicationDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		[HttpGet]
+		public async Task<IEnumerable<Tag>> Get()
+		{
+			return await _dbContext.Tags.ToListAsync();
+		}
+	}
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -16,20 +16,17 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
-app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html");
 
 app.Run();
